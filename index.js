8e6edb26437466b7a483a4b0d8aba4442e68e5cd
unknown <mehulkumardhusia@gmail.com>
2022-07-08 21:23:28 +0530

    Added a index.js file

---
+++ index.js
@@ -0,0 +1,120 @@
+const information = [
+    {
+        videoLink : 'https://www.youtube.com/watch?v=JaUCiKTNBxQ',
+        thumbnailImageLink : 'https://i.ytimg.com/vi/JaUCiKTNBxQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBZo0NcF2u8RRFbJu5W9_vIVCYqzA',
+        channelLogoLink : 'https://yt3.ggpht.com/C2DDl5zXvjBDZ8TAZbIAjAz4uxJGDt1wMDcn7n2_x_FDcahKBZf5-npIcx95Qde5GGm4yngAhA=s108-c-k-c0x00ffffff-no-rj',
+        heading : 'A day in the life of Sundar Pichai' ,
+        channelname : 'Mehul kumar dhusia' ,
+        view : '6.1M views • 3 Months Ago'
+    },
+    {
+        videoLink : 'https://www.youtube.com/watch?v=T2YcOXcaxeY',
+        thumbnailImageLink : 'https://img.youtube.com/vi/T2YcOXcaxeY/maxresdefault.jpg',
+        channelLogoLink : 'https://yt3.ggpht.com/ytc/AKedOLQ8PSwMRTU8HG_vDunTQ7eYgSqx9ju7vjDNe8dpXm8=s176-c-k-c0x00ffffff-no-rj-mo',
+        heading : 'iPhone 14 Series First Look - Crazy New Upgrades🔥🔥🔥' ,
+        channelname : 'Technical Guruji' ,
+        view : '6.1M views • 3 Months Ago'
+    },
+    {
+        videoLink : 'https://www.youtube.com/watch?v=sAzlWScHTc4',
+        thumbnailImageLink : 'https://img.youtube.com/vi/sAzlWScHTc4/maxresdefault.jpg',
+        channelLogoLink : 'https://yt3.ggpht.com/QHluaMJmoBWoDssKk40J_dyCs2h6mmfc46VhBeZYBn3Hgy9dhP-Gna_2cUYNAn6Ug8lpcA2GHaE=s176-c-k-c0x00ffffff-no-rj-mo',
+        heading : 'Naacho Naacho (Full Video) RRR - NTR, Ram Charan | M M Kreem | SS Rajamouli | Vishal Mishra & Rahul' ,
+        channelname : 'T-Series' ,
+        view : '6.1M views • 3 Months Ago'
+    },
+    {
+        videoLink : 'https://www.youtube.com/watch?v=5Eqb_-j3FDA',
+        thumbnailImageLink : 'https://img.youtube.com/vi/5Eqb_-j3FDA/maxresdefault.jpg',
+        channelLogoLink : 'https://yt3.ggpht.com/ytc/AKedOLRGa6COc1NMZToRC7R86s1PId85YlELWASKQxx2lw=s176-c-k-c0x00ffffff-no-rj',
+        heading : 'Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill' ,
+        channelname : 'Coke Studio' ,
+        view : '6.1M views • 3 Months Ago'
+    },
+    {
+        videoLink : 'https://www.youtube.com/watch?v=Ee56Iz5Yzp8',
+        thumbnailImageLink : 'https://img.youtube.com/vi/Ee56Iz5Yzp8/maxresdefault.jpg',
+        channelLogoLink : 'https://yt3.ggpht.com/ytc/AKedOLQW2HLXLYLS3HLdkSu-tIm0ge-2h_p0P53T5yYMLQ=s48-c-k-c0x00ffffff-no-rj',
+        heading : 'How to be Truly Productive in Life ? Step by step Explanation' ,
+        channelname : 'Amman dhattarwal' ,
+        view : '6.1M views • 3 Months Ago'
+    },
+    {
+        videoLink : 'https://www.youtube.com/watch?v=WBqoJVEyJPU',
+        thumbnailImageLink : 'https://img.youtube.com/vi/WBqoJVEyJPU/maxresdefault.jpg',
+        channelLogoLink : 'https://yt3.ggpht.com/ASeIhTUpJVmGZHJLTeLaQrZ6avcYVGPCZ75Evx1tnNx1ojhMroc9qvXdlkIWsG6omp9bv3bGUg=s48-c-k-c0x00ffffff-no-rj',
+        heading : 'Badshah - Sajna | Say Yes To The Dress (Official Video) | Payal Dev' ,
+        channelname : 'discovery plus india' ,
+        view : '6.1M views • 3 Months Ago'
+    },
+    {
+        videoLink : 'https://www.youtube.com/watch?v=R4He_Gcn7cA',
+        thumbnailImageLink : 'https://img.youtube.com/vi/R4He_Gcn7cA/maxresdefault.jpg',
+        channelLogoLink : 'https://yt3.ggpht.com/ytc/AKedOLS7LEgSGNLCVyihKsVHPR2OR2TU1Be9nVwkSHooxg=s48-c-k-c0x00ffffff-no-rj',
+        heading : 'The Monster Song - KGF Chapter 2 | Adithi Sagar | Ravi Basrur | Yash | Sanjay Dutt | Prashanth Neel' ,
+        channelname : 'Homable Films' ,
+        view : '6.1M views • 3 Months Ago'
+    },
+    {
+        videoLink : 'https://www.youtube.com/watch?v=ardtvdR28SQ',
+        thumbnailImageLink : 'https://img.youtube.com/vi/ardtvdR28SQ/maxresdefault.jpg',
+        channelLogoLink : 'https://yt3.ggpht.com/ytc/AKedOLQkWcZ2665Q7lCu63yQIsDiwUc6qJPT0Xk36UtIpg=s48-c-k-c0x00ffffff-no-rj',
+        heading : 'FAMOUS WEDDING SHOW (FULL) 2022 - Quick Style' ,
+        channelname : 'TheQuickStyle' ,
+        view : '6.1M views • 3 Months Ago'
+    },
+    {
+        videoLink : 'https://www.youtube.com/watch?v=zxetdL2v1u8',
+        thumbnailImageLink : 'https://img.youtube.com/vi/zxetdL2v1u8/maxresdefault.jpg',
+        channelLogoLink : 'https://yt3.ggpht.com/ytc/AKedOLSJD6GimSf4Bb82AzHJa3IfGCGK92WaUVmyrt65hQ=s48-c-k-c0x00ffffff-no-rj',
+        heading : 'Fury of #NTR30 - Hindi | NTR | Koratala Siva | Anirudh Ravichander' ,
+        channelname : 'Jr NTR' ,
+        view : '6.1M views • 3 Months Ago'
+    }
+]
+
+let videoContainer = document.getElementById('video-container') ;
+
+information.forEach(data =>{
+    let link = document.createElement('a') ;
+    let video = document.createElement('div') ;
+    let videoThumbnail = document.createElement('div') ;
+    let videoDetail = document.createElement('div') ;
+    let author = document.createElement('div') ;
+    let title = document.createElement('div') ;
+    let thumbnailImage = document.createElement('img') ;
+    let channelLogo = document.createElement('img') ;
+    let heading = document.createElement('h3') ;
+    let channelname = document.createElement('p') ;
+    let view = document.createElement('span') ;
+
+    video.classList.add('video') ;
+    videoThumbnail.classList.add('video-thumbnail') ;
+    videoDetail.classList.add('video-details') ;
+    author.classList.add('author') ;
+    channelLogo.classList.add('video-icon') ;
+    title.classList.add('title') ;
+
+    link.href = data.videoLink ;
+    thumbnailImage.src = data.thumbnailImageLink ;
+    channelLogo.src = data.channelLogoLink ;
+    heading.innerHTML = data.heading ;
+    channelname.innerHTML = data.channelname ;
+    view.innerHTML = data.view ;
+
+    videoContainer.appendChild(link) ;
+    link.appendChild(video) ;
+    video.appendChild(videoThumbnail) ;
+    video.appendChild(videoDetail) ;
+    videoThumbnail.appendChild(thumbnailImage) ;
+    videoDetail.appendChild(author) ;
+    videoDetail.appendChild(title) ;
+    author.appendChild(channelLogo) ;
+    title.appendChild(heading) ;
+    title.appendChild(channelname) ;
+    title.appendChild(view) ;
+})
+
+
+
+
